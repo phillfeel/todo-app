e8274d0bfdfcb13e33f65c934c514c59b7345f55
PhilippBykov <phillfeel@yandex.ru>
2021-04-12 15:30:35 +0500

    3

--- server.js
+++ server.js
@@ -1,23 +1,11 @@
-const express = require("express");
-const favicon = require("express-favicon");
 const path = require("path");
-const port = process.env.PORT || 8080;
-
-// здесь у нас происходит импорт пакетов и определяется порт нашего сервера
+const express = require("express");
 const app = express();
-app.use(favicon(__dirname + "/build/favicon.png"));
-
-//здесь наше приложение отдаёт статику
-app.use(express.static(__dirname));
+const port = process.env.PORT || 8000;
 app.use(express.static(path.join(__dirname, "build")));
-
-//простой тест сервера
-app.get("/ping", function (req, res) {
-    return res.send("pong");
-});
-
-//обслуживание html
-app.get("/*", function (req, res) {
+app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.listen(port);
+app.listen(port, () => {
+    console.log("Server is up!");
+});
